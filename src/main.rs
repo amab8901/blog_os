33ac9e60b98ba2eaecc13da43fc2f5c5a16fa3d8
amab8901 <amab8901@protonmail.com>
2023-03-23 18:56:06 +0100

    finish

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,10 @@
 
 extern crate alloc;
 
-use alloc::{
-    boxed::Box,
-    vec,
-    vec::Vec,
-    rc::Rc,
-};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use blog_os::task::{Task, keyboard};
+use blog_os::task::executor::Executor;
 
 mod serial;
 mod vga_buffer;
@@ -34,26 +30,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-    
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-    
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
-
     #[cfg(test)]
     test_main();
-
-    println!("It did not crash!");
-    blog_os::hlt_loop();
+    
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 }
 
 #[cfg(not(test))]
@@ -98,4 +81,13 @@ where
         self();
         serial_println!("[ok]");
     }
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
